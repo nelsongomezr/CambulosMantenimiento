3b11d969521b47a7a4c654ade25be132983acce4
unknown <nelsonaldevaran@gmail.com>
2022-01-14 16:25:40 -0500

    creacion de footer u nav admin

---
+++ partials/footer.php
@@ -0,0 +1,19 @@
+
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Form -->
+    <section class="">
+
+    </section>
+    <!-- Section: Form -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-white" href="">NAGR</a>
+  </div>
+  <!-- Copyright -->
+</footer>
